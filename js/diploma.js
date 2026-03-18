84dc379d76955dfeba840916d99847ce57fa7820
jeremiaspt <85419158+jeremiaspt@users.noreply.github.com>
2026-03-18 21:58:50 +0000

    Update diploma.js

--- js/diploma.js
+++ js/diploma.js
@@ -1,3 +1,17 @@
+/* ===============================
+MENU MOBILE
+=============================== */
+
+function toggleMenu(){
+
+const menu = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+
+menu.classList.toggle("open");
+overlay.classList.toggle("show");
+
+}
+
 const PROVAS = [
 "25M LIVRES",
 "50M LIVRES",
